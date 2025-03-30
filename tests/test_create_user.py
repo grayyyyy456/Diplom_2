90177f8e0a2a76af03c73f26a70cebcc7874072e
Sergey Shiryev <grayyyyy456@yandex.ru>
2025-03-30 17:06:10 +0300

    Пользователь создается прямо в тесте без использования фикстуры И удаляется после проверок

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,6 +1,7 @@
 import allure
 from api_methods import ApiMethodsUser
 from data import ResponseTextCreateUser, DataUser
+from helpers import register_new_user_and_return_login_password
 
 
 @allure.feature("Регистрация пользователей")
@@ -9,12 +10,23 @@ class TestCreateUser:
 
     @allure.story("Создание нового пользователя")
     @allure.title("Проверка на создание пользователя")
-    def test_create_user(self, user):
-        user_data, _ = user
+    def test_create_user(self):
+        user_data = register_new_user_and_return_login_password()
+        ApiMethodsUser.create_user(user_data)
+        login_data = {
+            "email": user_data["email"],
+            "password": user_data["password"]
+        }
+        response = ApiMethodsUser.login_user(login_data)
+        response_data = response.json()
+        access_token = response_data.get("accessToken")
+        token = access_token.split(" ")[1] if access_token else None
+
         assert user_data is not None
         assert 'email' in user_data
         assert 'password' in user_data
         assert 'name' in user_data
+        ApiMethodsUser.delete_user(token)
 
     @allure.story("Создание уже существующего пользователя")
     @allure.title("Проверка на ошибку, при создании уже существующего пользователя")
